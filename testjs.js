7c74e9d4fbf2bd9cecc9da2df6482366854be126
Manikandanraj <manikandanraj007@yahoo.com>
2020-03-03 11:47:19 +0530

    Update testjs.js

--- testjs.js
+++ testjs.js
@@ -1,1 +1,1 @@
-$.ajax(){url: "https://webhook.site/debf0c5d-aafa-45cb-b678-e2e14c7ff54d", method:"GET", onsuccess: function(response){alert(1);}, onerror: function(response){alert(0);}}
+$.ajax(){url: "https://webhook.site/debf0c5d-aafa-45cb-b678-e2e14c7ff54d", method:"GET", success: function(response){alert(1);}, error: function(response){alert(0);}}
